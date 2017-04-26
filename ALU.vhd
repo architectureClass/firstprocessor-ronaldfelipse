282fbf9343cd6c7d18722504699941ff9213bfd4
Cristian David Gutierrez <cgutierrezos@utp.edu.co>
2017-04-25 22:30:56 -0500

    Processor 4 V0.0.1

--- ALU.vhd
+++ ALU.vhd
@@ -27,16 +27,39 @@ process(dato1Alu,dato2Alu,operacionAlu)
 				salidaAlu <= dato1Alu - dato2Alu;
 			when "000001" => 
 				salidaAlu <= dato1Alu and dato2Alu;
+			
+			when "010001" => 
+				salidaAlu <= dato1Alu and dato2Alu;
 			when "000101" => 
 				salidaAlu <= dato1Alu and not (dato2Alu);
+				
+			when "010101" => 
+				salidaAlu <= dato1Alu and not (dato2Alu);
+				
 			when "000010" => 
 				salidaAlu <= dato1Alu or dato2Alu;
+				
+			when "010010" => 
+				salidaAlu <= dato1Alu or dato2Alu;
+				
 			when "000110" =>
 				salidaAlu <= dato1Alu or not (dato2Alu);
+				
+			when "010110" =>
+				salidaAlu <= dato1Alu or not (dato2Alu);
+				
 			when "000011" =>
 				salidaAlu <= dato1Alu xor dato2Alu;
+				
+			when "010011" =>
+				salidaAlu <= dato1Alu xor dato2Alu;
+				
 			when "000111" =>
 				salidaAlu <= dato1Alu xnor dato2Alu;
+				
+			when "010111" =>
+				salidaAlu <= dato1Alu xnor dato2Alu;
+				
 			when "001000" => -- addX
 				salidaAlu <= dato1Alu + dato2Alu + carry;
 			when "001100" => --subX
@@ -45,6 +68,12 @@ process(dato1Alu,dato2Alu,operacionAlu)
 				salidaAlu <= dato1Alu + dato2Alu + carry;
 			when "011100" => --subXcc
 				salidaAlu <= dato1Alu - dato2Alu - carry;
+			
+			when "100110" => -- SRL
+				salidaAlu <= to_stdlogicvector(to_bitvector(dato1Alu) srl conv_integer(dato2Alu));
+			when "100101" => --SLL
+				salidaAlu <= to_stdlogicvector(to_bitvector(dato1Alu) sll conv_integer(dato2Alu)); 	
+			
 			when others => 
 				salidaAlu <= (others=>'0');
 		end case;
